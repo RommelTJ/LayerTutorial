bb87188302f202f097c4ff650d1c2a12dbbc88ca
Rommel Rico <rommeltj@gmail.com>
2016-11-08 14:02:34 -0800

    Start UI work

---
+++ LayerTutorial/LayerMainViewController.swift
@@ -0,0 +1,40 @@
+//
+//  LayerMainViewController.swift
+//  LayerTutorial
+//
+//  Created by Rommel Rico on 11/7/16.
+//  Copyright © 2016 Rommel Rico. All rights reserved.
+//
+
+import UIKit
+
+class LayerMainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+extension LayerMainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello World"
+        return cell
+    }
+}
+
+extension LayerMainViewController: UITableViewDelegate {
+    
+}
